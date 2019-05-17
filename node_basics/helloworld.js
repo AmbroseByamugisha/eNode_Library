eb028ecfc883525dff26721c7c95d5da0967c723
AmbroseByamugisha <ambrosebyamugisha@gmail.com>
2019-05-17 12:51:26 +0300

    added hello world app

---
+++ node_basics/helloworld.js
@@ -0,0 +1,14 @@
+//load the http module
+var http = require('http');
+
+//create http server
+http.createServer(function(req, res){
+
+  //content header
+  res.writeHead(200, {'content-type': 'text/plain'});
+
+  //write message and signal communication is complete
+  res.end("Hello, world!\n");
+}).listen(8124);
+
+console.log('Server running on 8124');

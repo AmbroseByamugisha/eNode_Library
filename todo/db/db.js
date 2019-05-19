19624c5058a81729f9843a9971b929dc3f291c3a
AmbroseByamugisha <ambrosebyamugisha@gmail.com>
2019-05-19 09:47:18 +0300

    added the dummy database file

---
+++ todo/db/db.js
@@ -0,0 +1,14 @@
+const todos = [
+  {
+    id: 1,
+    title: "lunch",
+    description: "Go for lunch by 2pm"
+  },
+  {
+    id: 2,
+    title: "breakfast",
+    description: "Go for lunch by 3am"
+  },
+];
+
+export default todos;

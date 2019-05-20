028d34c6ad222e6a0501923efecb220f4fc85f1d
AmbroseByamugisha <ambrosebyamugisha@gmail.com>
2019-05-20 17:27:49 +0300

    added jwt token auth

--- node_express_tutorial/server.js
+++ node_express_tutorial/server.js
@@ -4,24 +4,27 @@ import dotenv from 'dotenv';
 import 'babel-polyfill';
 import ReflectionWithJsObject from './src/usingJSObject/controllers/Reflection';
 import ReflectionWithDB from './src/usingDB/controllers/Reflection';
+import UserWithDb from './src/usingDB/controllers/Users';
+import Auth from './src/usingDB/middleware/Auth';
 
 dotenv.config();
+const Reflection = process.env.TYPE === 'db' ? ReflectionWithDB : ReflectionWithJsObject;
 const app = express()
 
 app.use(express.json())
-
-process.env.TYPE = 'db';
-const Reflection = process.env.TYPE === 'db' ? ReflectionWithDB : ReflectionWithJsObject;
 
 app.get('/', (req, res) => {
   return res.status(200).send({'message': 'YAY! Congratulations! Your first endpoint is working'});
 });
 
-app.post('/api/v1/reflections', Reflection.create);
-app.get('/api/v1/reflections', Reflection.getAll);
-app.get('/api/v1/reflections/:id', Reflection.getOne);
-app.put('/api/v1/reflections/:id', Reflection.update);
-app.delete('/api/v1/reflections/:id', Reflection.delete);
+app.post('/api/v1/reflections', Auth.verifyToken, Reflection.create);
+app.get('/api/v1/reflections', Auth.verifyToken, Reflection.getAll);
+app.get('/api/v1/reflections/:id', Auth.verifyToken, Reflection.getOne);
+app.put('/api/v1/reflections/:id', Auth.verifyToken, Reflection.update);
+app.delete('/api/v1/reflections/:id', Auth.verifyToken, Reflection.delete);
+app.post('/api/v1/users', UserWithDb.create);
+app.post('/api/v1/users/login',UserWithDb.login);
+app.delete('/api/v1/users/me', Auth.verifyToken, UserWithDb.delete);
 
 app.listen(3000)
 console.log('app running on port ', 3000);

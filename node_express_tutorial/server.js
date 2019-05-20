6946f19d2a272e6eb3f46add91d2af6dc5407bc2
AmbroseByamugisha <ambrosebyamugisha@gmail.com>
2019-05-20 15:51:58 +0300

    added database configuration

--- node_express_tutorial/server.js
+++ node_express_tutorial/server.js
@@ -1,13 +1,27 @@
 // server.js
 import express from 'express';
+import dotenv from 'dotenv';
+import 'babel-polyfill';
+import ReflectionWithJsObject from './src/usingJSObject/controllers/Reflection';
+import ReflectionWithDB from './src/usingDB/controllers/Reflection';
 
+dotenv.config();
 const app = express()
 
 app.use(express.json())
 
+process.env.TYPE = 'db';
+const Reflection = process.env.TYPE === 'db' ? ReflectionWithDB : ReflectionWithJsObject;
+
 app.get('/', (req, res) => {
   return res.status(200).send({'message': 'YAY! Congratulations! Your first endpoint is working'});
-})
+});
+
+app.post('/api/v1/reflections', Reflection.create);
+app.get('/api/v1/reflections', Reflection.getAll);
+app.get('/api/v1/reflections/:id', Reflection.getOne);
+app.put('/api/v1/reflections/:id', Reflection.update);
+app.delete('/api/v1/reflections/:id', Reflection.delete);
 
 app.listen(3000)
 console.log('app running on port ', 3000);

3afb26d9e9eb040299a81f974b706b6baae9c376
AmbroseByamugisha <ambrosebyamugisha@gmail.com>
2019-05-25 10:20:00 +0300

    updated the classes inheritance file

--- node_basics/spread.js
+++ node_basics/spread.js
@@ -1,5 +1,37 @@
-var peaks = ["kilimanjaro", "Elgon", "Himalayas"]
-var canyons = ["ward", "blackwood"]
-var tahoe = [...peaks, ...canyons]
+// var peaks = ["kilimanjaro", "Elgon", "Himalayas"]
+// var canyons = ["ward", "blackwood"]
+// var tahoe = [...peaks, ...canyons]
 
-console.log(tahoe.join(', '))
+// console.log(tahoe.join(', '))
+
+// CLASSES
+class Vacation {
+    constructor(destination, length){
+        this.destination = destination
+        this.length = length
+    }
+
+    print(){
+        console.log(`${this.destination} will take ${this.length} days.`)
+    }
+}
+
+// var t = new Vacation("kampala", 23);
+// t.print()
+
+//Inheritance
+class Expedition extends Vacation{
+
+    constructor(destination, length, gear){
+        super(destination, length)
+        this.gear = gear
+    }
+
+    print(){
+        super.print()
+        console.log(`Bring your ${this.gear.join(" and your ")}`)
+    }
+}
+
+var r = new Expedition('Uganda', 34, ["gun", "cat"]);
+r.print()
